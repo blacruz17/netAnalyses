87495f6277507f17b1483e5e4cc4c7566dc19a12
Blan <76912800+blacruz17@users.noreply.github.com>
2025-03-27 16:36:37 +0100

    Update createPhyloseqFromCurated.R

--- createPhyloseqFromCurated.R
+++ createPhyloseqFromCurated.R
@@ -7,54 +7,6 @@ library(genefilter)
 
 rm(list = ls())
 
-# HELPER EXPORT FUNCTION #######################################################
-
-exportNet <- function(net, physeq, props, filename){
-  # EDGES
-  # Create edge object from the edge list exported by netConstruct()
-  edges <- net$edgelist1 %>%
-    select(v1, v2, asso) %>%
-    rename(Source = v1,
-           Target = v2,
-           Weight = asso) %>%
-    mutate(Type = "Undirected",
-           Sign = if_else(Weight < 0, "Negative", "Positive"))
-  
-  # NODES
-  # get taxonomic info:
-  taxa <- data.frame(physeq@tax_table) %>%
-    rownames_to_column(var = "Label")
-  
-  # get mean abundances:
-  mean_ab <- data.frame(
-    "Abundance" = apply(data.frame(physeq@otu_table), 1, mean)) %>%
-    rownames_to_column(var = "Label")
-  
-  # get clusters:
-  clusters <- data.frame("Cluster" =  props$clustering$clust1) %>%
-    rownames_to_column(var = "Label")
-  
-  # get hubs:
-  hubs <- data.frame(isHub = rep(1, length(props$hubs$hubs1)),
-                     "Label" = props$hubs$hubs1)
-  
-  # join all of them together:
-  metadata <- taxa %>%
-    left_join(mean_ab, by = "Label") %>%
-    left_join(clusters, by = "Label") %>%
-    left_join(hubs, by = "Label") %>%
-    mutate(isHub = if_else(is.na(isHub), 0, 1))
-  
-  
-  # WRITE CSV FILES:
-  write_csv(edges,
-            file = paste0(filename, "_edges.csv"),
-            quote = "needed")
-  write_csv(metadata,
-            paste0(filename, "_metadata.csv"),
-            quote = "needed")
-}
-
 # GET PHYLOSEQ OBJECTS #########################################################
 #### KarlssonFH_2013 ###########################################################
 physeq.karlsson <- sampleMetadata %>%
